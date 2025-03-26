78ae3d960dce956d23299350ede1c01658233209
spirits <hard_liquor@outlook.com>
2025-03-26 14:51:18 +0800

    check

--- app.ts
+++ app.ts
@@ -1,16 +1,21 @@
 import {addAliases} from 'module-alias';
 addAliases({
-    '@root': _dirname,
-    '@interfaces': '${_dirname}/interface',
-    '@config': '${_dirname}/config',
-    '@middlewares': '${_dirname}/middlewares',
+    '@root': __dirname,
+    '@interfaces': '${__dirname}/interface',
+    '@config': '${__dirname}/config',
+    '@middlewares': '${__dirname}/middlewares',
 })
+import Koa from 'koa';
+import config from '@config/index';
+import render from 'koa-swig';
+import serve from 'koa-static';
+import co from 'co';
 import { createContainer, Lifetime } from 'awilix';
 import { loadControllers } from 'node_modules/awilix-koa/lib/controller';
 import { scopePerRequest } from 'node_modules/awilix-koa/lib/scope-per-request';
 const container = createContainer();
 
-container.loadModules(['${_dirname}/service/*.ts'], {
+container.loadModules(['${__dirname}/service/*.ts'], {
     formatName: 'camelCase',
     resolverOptions:{
         lifetime: Lifetime.SCOPED
@@ -20,4 +25,4 @@ container.loadModules(['${_dirname}/service/*.ts'], {
 // 
 app.use(scopePerRequest(container));
 // 加载所有路由
-app.use(loadControllers('${_dirname/routers/*.ts}'));
+app.use(loadControllers('${__dirname/routers/*.ts}'));
